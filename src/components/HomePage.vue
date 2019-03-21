667bdcde041dd17a3db7b0ed904cf6a08d1b8747
Zarko Simic <zarko.zare.simic@gmail.com>
2019-03-21 23:21:05 +0100

    Added HomePage with NavBar menu

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,28 +1,111 @@
 <template>
-  <div class="hello">
+  <div class="container">
+    <div class="navMenu">
+      <img src="../assets/logo.png" class="logo"/>
+      <div class="searchMenu">
+        <input
+          class="searchInput"
+          type="text"
+          placeholder="Search..."
+          v-model="tags"
+          v-on:keyup.enter="search">
+        <button
+          class="searchButton"
+          v-on:click="search">Search</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  data: () => ({
+    items: [],
+    tags: ''
+  }),
+  mounted () {
+    $.ajax({
+      url: 'https://api.flickr.com/services/feeds/photos_public.gne?format=json&jsoncallback=?',
+      dataType: 'json'
+    }).then((data) => {
+      this.items = data.items
+      // eslint-disable-next-line
+      console.log("Items : " + JSON.stringify(this.items))
+    })
+  },
+  methods: {
+    search: function () {
+      const tags = this.tags.split(/ |,/).map($.trim).filter((el) => !!el.length).join(',')
+      $.ajax({
+        url: `https://api.flickr.com/services/feeds/photos_public.gne?tags=${tags}&format=json&jsoncallback=?`,
+        dataType: 'json'
+      }).then((data) => {
+        this.items = data.items
+        // eslint-disable-next-line
+        console.log("Tags : " + JSON.stringify(this.tags))
+        // eslint-disable-next-line
+        console.log("Items from search : " + JSON.stringify(this.items))
+      })
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  text-align: left;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.navMenu {
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  background-color: silver;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.logo {
+  width: 5rem;
+  height: 2rem;
+  display: flex;
+  padding: 8px;
 }
-a {
-  color: #42b983;
+.searchMenu {
+  display: flex;
+  min-height: 43px;
+  padding: 3px 0;
+  width: 40%;
+}
+.searchInput {
+  box-sizing: border-box;
+  font-size: 20px;
+  display: block;
+  padding: 3px 8px;
+  margin-right: 10px;
+  border-radius: 3px;
+  border: 1px solid #ccc;
+  width: 85%;
+}
+.searchButton {
+  font-size: 20px;
+  display: block;
+  padding: 3px 8px;
+  border-radius: 3px;
+  border: 1px solid #888;
+  background-color: #f1f1f1;
+  color: #333;
+}
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+}
+@media (max-width: 728px) {
+  .navMenu {
+    flex-direction: column;
+  }
+  .logo {
+    margin-bottom: 10px;
+  }
 }
 </style>
